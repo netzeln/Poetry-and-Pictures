3f4acf25595f4f3d123327488baaccadcd466743
cassandra culkins, hillary hintzen and nic netzel <hillary07@gmail.com>
2016-04-12 11:12:46 -0700

    add ability to get a random image based on array size

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,11 +4,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     var key = config.myApiKey;
-    var url = 'http://api.thewalters.org/v1/objects?keyword=spoon&apikey=JiOzztzq8vkwrYiX86LDMf1SEt9nfKCD1a38UdcAw2B1nO2FcM6hW9Gegvl5kCBn';
+    var url = 'http://api.thewalters.org/v1/objects?keyword=landscape&apikey=JiOzztzq8vkwrYiX86LDMf1SEt9nfKCD1a38UdcAw2B1nO2FcM6hW9Gegvl5kCBn';
 var test = Ember.$.getJSON(url);
     return Ember.$.getJSON(url).then(function(responseJSON) {
       console.log(responseJSON.Items);
-      return responseJSON.Items;
+      var numberOfResults = responseJSON.Items.length;
+      var randomResult = Math.floor(Math.random()*numberOfResults);
+      return responseJSON.Items[randomResult];
     });
   }
 });
