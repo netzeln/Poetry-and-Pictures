b37e8e08c468e8257467643ecf883e693d56c450
cassandra culkins, hillary hintzen and nic netzel <cassy.culkins@gmail.com>
2016-04-12 10:01:36 -0700

    add initial files

---
+++ app/routes/index.js
@@ -0,0 +1,14 @@
+import config from '../config/environment';
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model: function() {
+    var key = config.myApiKey;
+    var url = 'http://api.thewalters.org/v1/objects?keyword=spoon&apikey=JiOzztzq8vkwrYiX86LDMf1SEt9nfKCD1a38UdcAw2B1nO2FcM6hW9Gegvl5kCBn';
+var test = Ember.$.getJSON(url);
+    return Ember.$.getJSON(url).then(function(responseJSON) {
+      console.log(responseJSON.Items[0].Images);
+      return responseJSON.results;
+    });
+  }
+});
