a06ee017e23a61f528d52a55b058fa453dfa6792
cassandra culkins, hillary hintzen and nic netzel <cassy.culkins@gmail.com>
2016-04-12 10:34:53 -0700

    add feature to list images based on a keyword

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,8 +7,8 @@ export default Ember.Route.extend({
     var url = 'http://api.thewalters.org/v1/objects?keyword=spoon&apikey=JiOzztzq8vkwrYiX86LDMf1SEt9nfKCD1a38UdcAw2B1nO2FcM6hW9Gegvl5kCBn';
 var test = Ember.$.getJSON(url);
     return Ember.$.getJSON(url).then(function(responseJSON) {
-      console.log(responseJSON.Items[0].Images);
-      return responseJSON.results;
+      console.log(responseJSON.Items);
+      return responseJSON.Items;
     });
   }
 });
