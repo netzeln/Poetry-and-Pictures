a71df9dfe4c39d5e88bd6cced550b9baebc5ec8a
cassandra culkins, hillary hintzen and nic netzel <hillary07@gmail.com>
2016-04-12 13:59:42 -0700

    readd randomizer

--- app/routes/results.js
+++ app/routes/results.js
@@ -9,8 +9,10 @@ export default Ember.Route.extend({
     var url = 'https://jsonp.afeld.me/?url=https://pixabay.com/api/?key=2381172-26b42eade332e81b615568b91'+ testWord;
 
     return Ember.$.getJSON(url).then(function(responseJSON) {
-      console.log(responseJSON.hits[0]);
-      return responseJSON.hits[0];
+      var numberOfResults = responseJSON.hits.length;
+      var randomResult = Math.floor(Math.random()*numberOfResults);
+console.log(responseJSON.hits[randomResult]);
+      return responseJSON.hits[randomResult];
     });
   }
 });
