d6990115bfad3e6f0f013572cb7b3c01791b7b07
cassandra culkins, hillary hintzen and nic netzel <hillary07@gmail.com>
2016-04-13 09:16:53 -0700

    add unworking poem form

--- app/models/picture.js
+++ app/models/picture.js
@@ -4,6 +4,10 @@ export default Model.extend({
   imageApiId: DS.attr(),
   imgWebUrl: DS.attr(),
   imgPageUrl: DS.attr(),
+  previewUrl: DS.attr(),
   poems: DS.hasMany('poem', {async: true})
 
 });
+
+
+https://pixabay.com/api/?key=2381172-26b42eade332e81b615568b91&id=1268609
--- app/routes/results.js
+++ app/routes/results.js
@@ -4,13 +4,14 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   model: function(params) {
-          var key = config.myApiKey;
 
+    var key = config.myApiKey;
           if(params.params !== ""){
             var testWord = encodeURIComponent('&q='+ params.params + '&image_type=photo');
-            var url = 'https://jsonp.afeld.me/?url=https://pixabay.com/api/?key=2381172-26b42eade332e81b615568b91'+ testWord;
+            var url = 'https://jsonp.afeld.me/?url=https://pixabay.com/api/?key=2381172-26b42eade332e81b615568b91' + testWord;
+            
           }else{
-            var url = 'https://jsonp.afeld.me/?url=https://pixabay.com/api/?key=2381172-26b42eade332e81b615568b91'
+            var url = 'https://jsonp.afeld.me/?url=https://pixabay.com/api/?key=2381172-26b42eade332e81b615568b91'+ key;
           }
           return Ember.$.getJSON(url).then(function(responseJSON) {
             var numberOfResults = responseJSON.hits.length;
