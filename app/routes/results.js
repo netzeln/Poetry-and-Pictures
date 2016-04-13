e2d9999123fb7a5f19268f7d8072e1f455103542
cassandra culkins and hillary hintzen <cassy.culkins@gmail.com>
2016-04-13 14:38:29 -0700

    fix syntax errors

--- app/routes/results.js
+++ app/routes/results.js
@@ -3,40 +3,38 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-  model: function(params) {
+    model: function(params) {
 
-    var key = config.myApiKey;
-    if(params.params !== ""){
-      var testWord = encodeURIComponent('&q='+ params.params + '&image_type=photo');
-      var url = 'https://jsonp.afeld.me/?url=https://pixabay.com/api/?key=2381172-26b42eade332e81b615568b91' + testWord;
+      var key = config.myApiKey;
+      if(params.params !== ""){
+        var testWord = encodeURIComponent('&q='+ params.params + '&image_type=photo');
+        var url = 'https://jsonp.afeld.me/?url=https://pixabay.com/api/?key=2381172-26b42eade332e81b615568b91' + testWord;
 
-    }else{
-      var url = 'https://jsonp.afeld.me/?url=https://pixabay.com/api/?key=2381172-26b42eade332e81b615568b91';
+      }else{
+        var url = 'https://jsonp.afeld.me/?url=https://pixabay.com/api/?key=2381172-26b42eade332e81b615568b91';
+      }
+      return Ember.$.getJSON(url).then(function(responseJSON) {
+        var numberOfResults = responseJSON.hits.length;
+        var randomResult = Math.floor(Math.random()*numberOfResults);
+        console.log("route", responseJSON.hits[randomResult]);
+        return responseJSON.hits[randomResult];
+      });
+    },
+
+    actions: {
+    addPicture(result) {
+
+
+      var params = {
+        imageApiId: result.id,
+        imgWebUrl: result.webformatURL,
+        imgPageUrl: result.pageURL,
+        previewUrl: result.previewURL
+      };
+
+      var newPicture = this.store.createRecord('picture', params);
+      newPicture.save();
+      this.transitionTo('display', newPicture);
     }
-    return Ember.$.getJSON(url).then(function(responseJSON) {
-      var numberOfResults = responseJSON.hits.length;
-      var randomResult = Math.floor(Math.random()*numberOfResults);
-      console.log("route", responseJSON.hits[randomResult]);
-      return responseJSON.hits[randomResult];
-    });
-  },
-
-  actions: {
-  addPicture(result) {
-
-
-    var params = {
-      imageApiId: result.id,
-      imgWebUrl: result.webformatURL,
-      imgPageUrl: result.pageURL,
-      previewUrl: result.previewURL
-    };
-
-    var newPicture = this.store.createRecord('picture', params);
-    newPicture.save();
-    this.transitionTo('display', newPicture);
   }
-}
-
-
-// savePoem action will be sent up here if the form remains on this page
+})
