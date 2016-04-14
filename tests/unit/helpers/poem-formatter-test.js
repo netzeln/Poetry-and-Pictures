773690e8c5aaec4e78f9982e63cfd9247790a64f
brad pizzimenti, cassandra culkins, hillary hintzen and nic netzel <hillary07@gmail.com>
2016-04-14 15:09:07 -0700

    add helper function for haiku display (potentially)

---
+++ tests/unit/helpers/poem-formatter-test.js
@@ -0,0 +1,10 @@
+import { poemFormatter } from 'poetry-pictures/helpers/poem-formatter';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | poem formatter');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = poemFormatter([42]);
+  assert.ok(result);
+});
